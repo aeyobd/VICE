76b7947666fa81b7d236ea5ac09100af21ab449b
astrobeard <giganano9@gmail.com>
2022-01-13 16:51:44 -0500

    [ci skip] maint: minor cleanup

--- setup.py
+++ setup.py
@@ -544,6 +544,13 @@ def find_package_data():
 	Extensions
 	----------
 	.dat : files holding built-in data
+	.obj : a pickled object -> currently the only instance is the pickled
+		dictionary containing version info of build dependencies
+
+	VICE's C extensions are compiled individually and wrapped into a
+	shared object using make. All of this output is moved to the install
+	directory to allow forward compatibility with future features that may
+	require it.
 	"""
 	packages = find_packages()
 	data = {}
